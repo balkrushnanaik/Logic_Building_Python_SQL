e4d580f53a129712fa28a45e140dd8fddc97e78d
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-22 20:17:04 +0530

    Basic Programs

--- Python/Basics/leap_year.py
+++ Python/Basics/leap_year.py
@@ -1,1 +1,10 @@
-# Remaining
+year = int(input('Enter a year: '))
+
+if year % 100 == 0 and year % 400 == 0:
+    print(f'Year {year} is a leap year')
+elif year % 4 == 0 and year % 100 != 0:
+    print(f'Year {year} is a leap year')
+else:
+    print(f'Year {year} is not leap year')
+
+print('Thank you!')
