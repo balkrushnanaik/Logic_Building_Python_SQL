6141cf0c2b5f1b5c79999c7a6911c720b35dab21
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-28 22:48:51 +0530

    Basic Python Program

---
+++ Python/Basics/addition using function.py
@@ -0,0 +1,12 @@
+# 56. Write a Python program to create a function for addition of two numbers.
+#
+def add(a,b):
+    return a + b
+c = add(10,30)
+print(c)
+
+def addition(a,b=20):
+    c = a + b
+    print(c)
+addition(10,20)
+
