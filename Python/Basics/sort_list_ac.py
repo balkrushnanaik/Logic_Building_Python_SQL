3d9c505bef51ee1fa283a86e13a5fc645be7d87b
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-05-30 23:27:37 +0530

    Basic Python Program

---
+++ Python/Basics/sort_list_ac.py
@@ -0,0 +1,8 @@
+#  65. Write a Python program to sort a list in ascending order.
+
+
+list1 = [1,2,3,1,2,3,4,5,6,6,9,1,2,3,4,6,3,2,2]
+print(sorted(list1))
+
+
+
---
+++ Python/Basics/sum_of_list.py
@@ -0,0 +1,7 @@
+# 64. Write a Python program to calculate the sum of list elements.
+
+list1 = [1,2,3,4,5,6]
+print(list1)
+
+list2 = sum(list1)
+print(f'The sum of all list element is : {list2}')
