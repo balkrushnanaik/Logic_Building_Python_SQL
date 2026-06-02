0161677090307402cb3568e051686420042f1d94
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-02 22:10:20 +0530

    Basic Python Program

---
+++ Python/Basics/concatenate_two_strings.py
@@ -0,0 +1,17 @@
+# 53. Write a Python program to concatenate two strings.
+
+# Method 1
+
+# str1 = 'Balkrushna'
+# str2 = 'Naik'
+#
+# str3 = str1 + str2
+# print(str3)
+
+# Method 2
+str1 = 'Balkrushna'
+str2 = 'Naik'
+
+str3 = ''.join([str1, str2])
+
+print(str3)
