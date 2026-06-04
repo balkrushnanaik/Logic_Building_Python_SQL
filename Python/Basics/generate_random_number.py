57c0fd6494ae9147c54b860a38ef5688e5e4eb7d
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-04 23:04:00 +0530

    Basic Python Program

--- Python/Basics/generate_random_number.py
+++ Python/Basics/generate_random_number.py
@@ -1,5 +1,10 @@
 # 82. Write a Python program to generate random numbers.
 import random
+while True:
+    numbers = random.randint(1, 100)
+    print(numbers)
 
-numbers = random.randint(1,100)
-print(numbers)
+    if numbers > 90:
+        break
+
+
