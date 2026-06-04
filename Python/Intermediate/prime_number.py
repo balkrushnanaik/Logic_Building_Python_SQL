8d3e5c7da9bc591d9269cabb9282623169558f32
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-04 21:35:16 +0530

    Intermediate Python Program

---
+++ Python/Intermediate/prime_number.py
@@ -0,0 +1,17 @@
+# Program to check whether a number is prime or not
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print(num, "is not a Prime Number")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print(num, "is not a Prime Number")
+            break
+    else:
+        print(num, "is a Prime Number")
+
+
+
+
