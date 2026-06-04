501709b9533fc30dbde01f01617d736d25e3c5d1
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-04 21:40:11 +0530

    Intermediate Python Program

---
+++ Python/Intermediate/prime_number_between 1 to 100.py
@@ -0,0 +1,12 @@
+# 77. Write a Python program to print all prime numbers between 1 to 100.
+
+for num in range(2, 101):
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, end=" ")
