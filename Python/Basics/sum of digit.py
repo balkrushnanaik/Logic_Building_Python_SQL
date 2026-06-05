2172553988434e61214f789722f1eb5c6f15c4ff
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-05 22:33:46 +0530

    Basic Python Program

---
+++ Python/Basics/sum of digit.py
@@ -0,0 +1,14 @@
+# Find the sum of digits of a number
+
+num = int(input("Enter a number: "))
+
+sum_digits = 0
+
+num = abs(num)  # Handles negative numbers
+
+while num > 0:
+    digit = num % 10
+    sum_digits += digit
+    num = num // 10
+
+print("Sum of digits:", sum_digits)
