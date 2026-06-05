5015dda73734a8d9d2387b2e5dd787c7652675cb
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-05 22:32:47 +0530

    Basic Python Program

---
+++ Python/Basics/reverse number using loops.py
@@ -0,0 +1,16 @@
+# 37. Write a Python program to reverse a number using loops.
+
+
+
+
+num = int(input("Enter a number: "))
+
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reversed number:", reverse)
+
