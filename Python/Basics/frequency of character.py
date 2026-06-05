d3f26919e4b31fcdbdb1c0c98b4dd2fb6208c04e
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-05 22:19:50 +0530

    Basic Python Program

---
+++ Python/Basics/frequency of character.py
@@ -0,0 +1,15 @@
+# Count frequency of characters in a string
+
+text = input("Enter a string: ")
+
+freq = {}
+
+for char in text:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+
+print("Character Frequencies:")
+for char, count in freq.items():
+    print(char, ":", count)
