d61d5eceaa2f6ce0a5b1adb1d2d17c1c990a35f7
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-06 22:09:02 +0530

    Basic Python Program

---
+++ Python/Basics/character upper lower conditional.py
@@ -0,0 +1,10 @@
+# 29. Write a Python program to check whether a character is uppercase or lowercase.
+
+name = 's'
+
+if name.isupper():
+    print('Upper')
+elif name.islower():
+    print('Lower')
+else:
+    print('Invalid')
