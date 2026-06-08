0fc8410c5b9327ae79661e4851329888192774f6
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-08 22:25:43 +0530

    Intermediate Queries on Employee Management

--- SQL/Employee Management.sql
+++ SQL/Employee Management.sql
@@ -19,11 +19,14 @@ SELECT * FROM employee
 WHERE salary > 50000;
 
 -- 2. Find employees working in the "IT" department.
-SELECT *
+CREATE TABLE emp_department AS
+SELECT e.emp_id, e.emp_name, e.salary, e.city, e.hire_date, e.dept_id, d.dept_name
 FROM employee as e
 INNER JOIN department as d
 ON e.dept_id = d.dept_id
 WHERE dept_name = 'IT';
+
+-- DROP TABLE Emp_Department;
 
 -- 3. Display the top 5 highest-paid employees.
 
@@ -31,8 +34,11 @@ SELECT * FROM employee
 ORDER BY salary DESC LIMIT 5;
 
 
-
-
+-- 4. Count the number of employees in each department.
+SELECT * FROM emp_department;
+SELECT dept_name, COUNT(*) AS emp_in_each_dept
+FROM Emp_Department
+GROUP BY dept_name;
 
 
 
