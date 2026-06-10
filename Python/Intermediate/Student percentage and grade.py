9251cc4b82be35fca4d869903f8c8cf6610457b0
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-10 22:49:42 +0530

    Intermediate Python Program

---
+++ Python/Intermediate/Student percentage and grade.py
@@ -0,0 +1,28 @@
+# Program to calculate student percentage and grade
+
+m1 = float(input("Enter marks of Subject 1: "))
+m2 = float(input("Enter marks of Subject 2: "))
+m3 = float(input("Enter marks of Subject 3: "))
+m4 = float(input("Enter marks of Subject 4: "))
+m5 = float(input("Enter marks of Subject 5: "))
+
+total = m1 + m2 + m3 + m4 + m5
+percentage = total / 5
+
+print("Total Marks =", total)
+print("Percentage =", percentage)
+
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 80:
+    grade = "A"
+elif percentage >= 70:
+    grade = "B"
+elif percentage >= 60:
+    grade = "C"
+elif percentage >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade =", grade)
