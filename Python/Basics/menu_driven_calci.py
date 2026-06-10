1be1601669da9ce55a9e758209b4e40b452dd85b
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-10 22:44:04 +0530

    Basic Python Program

---
+++ Python/Basics/menu_driven_calci.py
@@ -0,0 +1,36 @@
+# Menu-Driven Calculator
+
+while True:
+    print("\n----- Calculator Menu -----")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice (1-5): "))
+
+    if choice == 5:
+        print("Exiting Calculator...")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == 1:
+        print("Result =", num1 + num2)
+
+    elif choice == 2:
+        print("Result =", num1 - num2)
+
+    elif choice == 3:
+        print("Result =", num1 * num2)
+
+    elif choice == 4:
+        if num2 != 0:
+            print("Result =", num1 / num2)
+        else:
+            print("Division by zero is not allowed.")
+
+    else:
+        print("Invalid Choice!")
