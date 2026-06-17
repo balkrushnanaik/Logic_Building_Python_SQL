27db08a702eab7ffe2e6958eaf4d3863d5c20306
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-17 19:29:15 +0530

    Learned Enumerate Function in Python

---
+++ Python/Intermediate/enumerate_functions.py
@@ -0,0 +1,9 @@
+from turtledemo.penrose import start
+
+lst = ['Soumya','Ramya','Vaibhav','Sidharth']
+
+for i, v in enumerate(lst):
+    print(i,v)
+
+for i in range(len(lst)):
+    print(i,lst[i])
