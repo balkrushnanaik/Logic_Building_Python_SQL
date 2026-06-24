c2cd548200a0873ac6a8410dd00f390332d07525
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-06-24 19:38:07 +0530

    Solving SQL Leetcode Problems

---
+++ SQL/Leetcode_average_selling_price_problem.sql
@@ -0,0 +1,36 @@
+USE leetcode;
+
+CREATE TABLE Prices (
+    product_id INT,
+    start_date DATE,
+    end_date DATE,
+    price INT,
+    PRIMARY KEY (product_id, start_date, end_date)
+);
+
+INSERT INTO Prices (product_id, start_date, end_date, price) VALUES
+(1, '2019-02-17', '2019-02-28', 5),
+(1, '2019-03-01', '2019-03-22', 20),
+(2, '2019-02-01', '2019-02-20', 15),
+(2, '2019-02-21', '2019-03-31', 30);
+
+CREATE TABLE UnitsSold (
+    product_id INT,
+    purchase_date DATE,
+    units INT
+);
+
+INSERT INTO UnitsSold (product_id, purchase_date, units) VALUES
+(1, '2019-02-25', 100),
+(1, '2019-03-01', 15),
+(2, '2019-02-10', 200),
+(2, '2019-03-22', 30);
+-- USE IFNULL()
+-- ROUND()
+SELECT p.product_id, IFNULL(ROUND(SUM(p.price * u.units)/SUM(u.units),2),0) AS average_price
+FROM Prices AS p
+LEFT JOIN UnitsSold AS u
+ON p.product_id = u.product_id
+AND u.purchase_date >= start_date
+AND u.purchase_date <= end_date
+GROUP BY p.product_id;
