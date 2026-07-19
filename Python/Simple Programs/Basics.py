1399f5a8d509a8eff88bb85ee1ad50180ba385bf
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-19 21:15:37 +0530

    Solving Python Basic Programs

---
+++ Python/Simple Programs/Basics.py
@@ -0,0 +1,35 @@
+# 1. Print "Hello, World!"
+print("Hello, World!")
+# 2. Print your name and age.
+name, age = "Balkrushna", 24
+print(name,age)
+# 3. Add two numbers.
+a = 20
+b = 30
+sum = a + b
+print(sum)
+# 4. Subtract two numbers.
+a = 20
+b = 30
+sub = a - b
+print(sub)
+# 5. Multiply two numbers.
+a = 20
+b = 30
+mul = a * b
+print(mul)
+# 6. Divide two numbers.
+# 7. Find remainder using `%`.
+# 8. Swap two variables.
+# 9. Find square of a number.
+# 10. Find cube of a number.
+# 11. Calculate simple interest.
+# 12. Calculate area of a rectangle.
+# 13. Calculate area of a circle.
+# 14. Convert Celsius to Fahrenheit.
+# 15. Convert Fahrenheit to Celsius.
+# 16. Convert kilometers to miles.
+# 17. Convert minutes into hours and minutes.
+# 18. Find ASCII value of a character.
+# 19. Take user input and display it.
+# 20. Calculate average of three numbers.
