0b3cb76a02dc5bf5541520ae067975c603af51c1
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-24 20:34:23 +0530

    Solving Python Problems

--- Python/Simple Programs/function_problems.py
+++ Python/Simple Programs/function_problems.py
@@ -8,5 +8,9 @@ print(f"The sum of 5 and 10 is {add}.")
 # 94. Create a function to check prime number.
 
 # 95. Create a function to calculate factorial.
-
+def factorial(n,fact):
+    for i in range(1,n+1):
+        fact = fact * i
+    print(f'The factorial of {n} is : {fact}')
+factorial(5,1)
 # 96. Create a recursive Fibonacci function.
