a0c223aea5517eb52c2463e1c55c5018b99bff31
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-28 21:32:30 +0530

    Solving Python Problems

--- Python/Simple Programs/loopsProgram.py
+++ Python/Simple Programs/loopsProgram.py
@@ -83,3 +83,7 @@ for i in range(1, num + 1):
     print()
 
 # 60. Print pyramid pattern.
+num = int(input("Enter the number of rows: "))
+for i in range(1, num + 1):
+    print(' ' * (num - i) + '*' * (2 * i - 1))
+    
