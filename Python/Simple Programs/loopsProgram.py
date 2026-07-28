eed6f043245d3252132eb035a57bbe175d9cf88a
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-28 21:32:58 +0530

    Solving Python Problems

--- Python/Simple Programs/loopsProgram.py
+++ Python/Simple Programs/loopsProgram.py
@@ -67,6 +67,14 @@ for _ in range(n):
     a, b = b, a + b
 
 # 54. Check prime number.
+num4 = int(input("Enter a number: "))
+if num4 > 1:
+    for i in range(2, int(num4 ** 0.5) + 1):
+        if num4 % i == 0:
+            print(f"{num4} is not a prime number.")
+            break
+    else:
+        print(f"{num4} is a prime number.")
 # 55. Print prime numbers between 1 and 100.
 # 56. Find GCD of two numbers.
 # 57. Find LCM of two numbers.
@@ -86,4 +94,3 @@ for i in range(1, num + 1):
 num = int(input("Enter the number of rows: "))
 for i in range(1, num + 1):
     print(' ' * (num - i) + '*' * (2 * i - 1))
-    
