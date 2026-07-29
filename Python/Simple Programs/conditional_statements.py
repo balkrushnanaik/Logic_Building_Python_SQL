e1495c69aaa166efca053abba7b57ffe34adbd4f
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-29 21:39:36 +0530

    Python Prgrams

--- Python/Simple Programs/conditional_statements.py
+++ Python/Simple Programs/conditional_statements.py
@@ -158,4 +158,11 @@ root2 = (-b - cmath.sqrt(discriminant)) / (2*a)
 print(f"The roots are {root1} and {root2}.")
 
 # 39. Check triangle validity.
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+if a + b > c and a + c > b and b + c > a:
+    print("The triangle is valid.") 
+else:
+    print("The triangle is not valid.")
 # 40. Find type of triangle.
