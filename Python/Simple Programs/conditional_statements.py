8b2e16642d228e0c5b14cba24a16562ae628b389
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-29 21:37:58 +0530

    Python Prgrams

--- Python/Simple Programs/conditional_statements.py
+++ Python/Simple Programs/conditional_statements.py
@@ -125,6 +125,14 @@ if 100 <= number <= 999:
 else:
     print(f"{number} is not a three-digit number.")
 # 36. Calculate electricity bill.
+units = float(input("Enter the number of units consumed: "))
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = 100 * 5 + (units - 100) * 10
+else:
+    bill = 100 * 5 + 100 * 10 + (units - 200) * 15      
+    
 
 # 37. Calculate income tax based on slabs.
 # 38. Find roots of a quadratic equation.
