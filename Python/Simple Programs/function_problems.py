121ba30140780d3c77f43b44bcd0f377738d2977
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-07-30 21:00:30 +0530

    Python Programs

--- Python/Simple Programs/function_problems.py
+++ Python/Simple Programs/function_problems.py
@@ -6,6 +6,15 @@ def add_numbers(a, b):
 add = add_numbers(5, 10)
 print(f"The sum of 5 and 10 is {add}.")
 # 94. Create a function to check prime number.
+num = 29
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+print(f"{num} is prime: {is_prime(num)}")
 
 # 95. Create a function to calculate factorial.
 def factorial(n,fact):
