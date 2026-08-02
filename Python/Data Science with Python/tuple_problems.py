17cab9b168cc8668951acbd76ee246554874b0de
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-08-02 22:21:04 +0530

    Solving Python Simple Programs

--- Python/Data Science with Python/tuple_problems.py
+++ Python/Data Science with Python/tuple_problems.py
@@ -11,6 +11,9 @@ print(f"Last element: {e}")
 tuple2 = (1, 2, 2, 3, 4, 5)
 print(f"Element 2 occurs {tuple2.count(2)} times in the tuple.")
 # 19. Find the index of a given element.
+element = 3
+index = tuple2.index(element)
+print(f"Index of element {element}: {index}")
 # 20. Convert a tuple into a list.
 # 21. Convert a list into a tuple.
 # 22. Check whether an element exists in a tuple.
