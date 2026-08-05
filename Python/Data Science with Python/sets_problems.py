2f64c0184c93472657d33b2cf191acbd39dc6803
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-08-05 17:20:58 +0530

    Solving Python Simple Programs

---
+++ Python/Data Science with Python/sets_problems.py
@@ -0,0 +1,15 @@
+# # 🔵 Set (10 Problems)
+
+# 26. Create a set of integers.
+set1 = {1, 2, 3, 4, 5}
+# 27. Add a new element to a set.
+set1.add(6)
+print(f"Set after adding an element: {set1}")
+# 28. Remove an element from a set.
+# 29. Find the union of two sets.
+# 30. Find the intersection of two sets.
+# 31. Find the difference between two sets.
+# 32. Check whether an element exists in a set.
+# 33. Remove duplicate values from a list using a set.
+# 34. Find elements present in one set but not in another.
+# 35. Check whether two sets are disjoint.
