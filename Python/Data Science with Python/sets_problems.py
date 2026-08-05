be3814fb3366280ead0a6af10961283098e9100a
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-08-05 17:21:33 +0530

    Solving Python Simple Programs

--- Python/Data Science with Python/sets_problems.py
+++ Python/Data Science with Python/sets_problems.py
@@ -9,6 +9,9 @@ print(f"Set after adding an element: {set1}")
 set1.remove(3)
 print(f"Set after removing an element: {set1}")
 # 29. Find the union of two sets.
+set2 = {4, 5, 6, 7, 8}
+union_set = set1.union(set2)
+print(f"Union of set1 and set2: {union_set}")
 # 30. Find the intersection of two sets.
 # 31. Find the difference between two sets.
 # 32. Check whether an element exists in a set.
