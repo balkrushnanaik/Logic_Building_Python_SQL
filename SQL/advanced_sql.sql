8c3b16b652a177c88f50627f22dbef95c74f9e41
balkrushnanaik0208 <balkrushnanaik9322@gmail.com>
2026-08-26 22:32:59 +0530

    Solving SQL Basic Problems

--- SQL/advanced_sql.sql
+++ SQL/advanced_sql.sql
@@ -562,7 +562,9 @@ SELECT *
 FROM students
 WHERE name LIKE '%r%';
 # 66. Display students whose names have exactly 5 characters.
-#
+SELECT *
+FROM students
+WHERE LENGTH(name) = 5;
 # 67. Display students whose city starts with the letter `P`.
 #
 # 68. Display students whose marks are between 50 and 80 **and** whose city is Pune.
